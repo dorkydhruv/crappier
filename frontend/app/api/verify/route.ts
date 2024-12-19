20f4c923837e8b234dafdcbeced9d6072b8f439a
Dhruv Sharma <thedhruvsharma@gmail.com>
2024-12-19 07:47:04 +0530

    refactor: verify route

--- frontend/app/api/verify/route.ts
+++ frontend/app/api/verify/route.ts
@@ -1,11 +1,51 @@
-import { validateToken } from "@/lib/functions/user/validateToken";
+import prisma from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (req: NextRequest) => {
-  if (!req.body) return new Response("Invalid request", { status: 400 });
+  if (!req.body)
+    return NextResponse.json({
+      response: false,
+    });
   const body = await req.json();
-  const response = validateToken(body.token);
+  const token = body.token;
+  if (!token)
+    return NextResponse.json({
+      response: false,
+    });
+  console.log("Validating token");
+  const tokenData = await prisma.verificationToken.findFirst({
+    where: {
+      token,
+      expiresAt: {
+        gte: new Date(),
+      },
+    },
+  });
+  if (!tokenData || !tokenData.email) {
+    return NextResponse.json({
+      response: false,
+    });
+  }
+  console.log(
+    "All checks passed successfully. Deleting token and updating user"
+  );
+  const email = tokenData.email;
+  await prisma.$transaction(async (tx) => {
+    await tx.verificationToken.delete({
+      where: {
+        id: tokenData.id,
+      },
+    });
+    await tx.user.update({
+      where: {
+        email,
+      },
+      data: {
+        verified: true,
+      },
+    });
+  });
   return NextResponse.json({
-    response,
+    response: true,
   });
 };
